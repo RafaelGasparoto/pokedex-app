f8163a710841499d7b19d032f73776e79e75d081
Rafael <rafaelgasparoto4@gmail.com>
2022-09-26 00:15:32 -0300

    separação das colunas e criação do header e footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'pokedex-app';
+  tiles: Tile[] = [
+    {id: 'id1', text: 'One', cols: 1, rows: 1, color: 'gray'},
+    {id: 'id2', text: 'Two', cols: 3, rows: 1, color: 'white'},
+    {id: 'id4', text: 'Three', cols: 1, rows: 1, color: 'gray'},
+  ];
+}
+
+export interface Tile {
+  id: string;
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
