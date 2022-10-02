3a2dba3be9e5f64a8a16787dd77ecdcae43c3f81
Rafael <rafaelgasparoto4@gmail.com>
2022-10-02 01:03:51 -0300

    button scroll go to up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,27 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'pokedex-app';
+  isShow: boolean = false
+  topPosToStartShowing = 1000;
+
+  @HostListener('window:scroll')
+  checkScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    console.log('[scroll]', scrollPosition);
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
 }
